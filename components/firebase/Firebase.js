7d1a7800ce990285ef59f89b9ef25d94b5e201f5
Cody Swain <swain.cody@gmail.com>
2018-11-27 16:16:38 -0800

    Added firebase functions

--- components/firebase/Firebase.js
+++ components/firebase/Firebase.js
@@ -1,29 +1,37 @@
 import * as firebase from 'firebase'
 import 'firebase/firestore';
 
-export const init = () => {
-  let config = {
-    apiKey: "AIzaSyAO8BB0W6mXDr3ihvkMn-uJjopgfsf_qow",
-    authDomain: "cherishly-412dd.firebaseapp.com",
-    databaseURL: "https://cherishly-412dd.firebaseio.com",
-    projectId: "cherishly-412dd",
-    storageBucket: "cherishly-412dd.appspot.com",
-    messagingSenderId: "378187715149"
-  }
-  if (!firebase.apps.length) {
-  	firebase.initializeApp(config) 
-	}
-	const firestore = firebase.firestore();
-	const settings = {timestampsInSnapshots: true};
-	firestore.settings(settings);
+class FireBase {
+  constructor() {
+    let config = {
+      apiKey: "AIzaSyAO8BB0W6mXDr3ihvkMn-uJjopgfsf_qow",
+      authDomain: "cherishly-412dd.firebaseapp.com",
+      databaseURL: "https://cherishly-412dd.firebaseio.com",
+      projectId: "cherishly-412dd",
+      storageBucket: "cherishly-412dd.appspot.com",
+      messagingSenderId: "378187715149"
+    }
+     if (!firebase.apps.length) {
+      firebase.initializeApp(config) 
+    }
+    const firestore = firebase.firestore();
+
+    // Random thing required by firestore
+    const settings = {timestampsInSnapshots: true};
+    firestore.settings(settings);
+  };
+
+  downloadData = () => {
+    //Download data here
+    console.log("Download data function called")
+  };
+
+  uploadData = (collectionId, documentId, data) => {
+    firebase.firestore().collection(collectionId).doc(documentId).set({
+      data,
+    })
+  };
 }
 
-export const putData = () => {
-  return (
-  	firebase.firestore().collection("sample").doc("Test").set({
-	    name: "Los Angeles",
-	    state: "CA",
-	    country: "USA"
-		})
-	);
-}
+Fire = new FireBase();
+export default Fire;
